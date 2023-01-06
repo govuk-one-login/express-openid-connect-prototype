02e7bc506cd1851deb080b9b7cf92e66b2b0c366
Chris Fryer <chris.fryer@digital.cabinet-office.gov.uk>
2023-01-06 14:36:11 +0000

    Integrate the Prototype Kit with GOV.UK One Login
    Required modules:
    
    - express-openid-connect
    - jsonweboken

---
+++ README.md
@@ -0,0 +1,84 @@
+# GOV.UK Prototype + express-openid-connect + GOV.UK One Login
+
+This proof-of-concept integrates the [GOV.UK Prototype Kit](https://prototype-kit.service.gov.uk/)
+with [GOV.UK One Login](https://www.sign-in.service.gov.uk/) using Auth0's
+[express-openid-connect](https://github.com/auth0/express-openid-connect) `npm` module.
+
+The user experience is hardly Alton Towers: you log in, you get your profile. But see [/app/routes.js](app/routes.js)
+to see how little configuration is required to get your Express prototype integrated as a Relying Party.
+
+## Environment variables
+
+The Prototype requires the following environment variables:
+
+    ISSUER_BASE_URL
+
+Likely to be https://oidc.integration.account.gov.uk or similar
+
+    BASE_URL
+
+The base URL of wherever you are hosting this prototype.  It should have been registered as a Relying Party with GOV.UK One Login.
+
+    CLIENT_ID
+
+The Client ID of your Relying Party, which you should have received when you registered with GOV.UK One Login.
+
+    SECRET
+
+A long, random string used to encrypt the session cooke.  You can generate one with `openssl rand -base64 32`
+
+    RSA_PRIVATE_KEY
+
+The private key of the keypair you generated when registering your Relying Party with GOV.UK One Login.  You can generate an RSA keypair as follows:
+
+    openssl genrsa -out private.pem 2048
+    openssl rsa -in private.pem -pubout
+
+(Obviously) only supply the public key to the OP.  The private key should remain just that.
+
+### Optional environment variables
+
+    REQUEST_IDENTITY_PROOFING
+
+Set this to the string `"true"` if you want Identity Proofing and Verification.  If you do, the following are also required:
+
+    CREDENTIAL_ISSUER_URL
+
+Likely to be https://identity.integration.account.gov.uk
+
+    SPOT_PUBLIC_KEY
+
+The RSA Public Key of the Credential Issuer (i.e. the above host)
+
+## Running locally
+
+Unless you really know what you're doing, you're going to find it hard to run a working Relying Party on localhost.  But to start the Prototype Kit:
+
+    npm install
+    npm run dev
+
+and browse to https://localhost:3000
+
+## Bonus: a quick round of code golf
+
+This is (almost) a complete Relying Party that will spit out your profile in JSON:
+
+```js
+const express = require('express')
+const app = express()
+const { auth, requiresAuth } = require('express-openid-connect')
+const jwt = require('jsonwebtoken')
+app.use(auth({
+  authorizationParams: {
+    response_type: 'code',
+    scope: 'openid email phone',
+  },
+  clientAssertionSigningKey: process.env.RSA_PRIVATE_KEY,
+  idTokenSigningAlg: 'ES256'
+}))
+app.get('/', async (req, res) => {
+  userinfo = await req.oidc.fetchUserInfo()
+  res.json(userinfo)
+})
+app.listen(8080)
+```
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,94 @@
-//
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-//
-
+/**
+ * This uses https://github.com/auth0/express-openid-connect
+ *
+ * It uses the `private_key_jwt` token endpoint authentication method
+ * and expects responses to be signed with ECDSA using P-256 and SHA-256
+ *
+ * Required environment variables:
+ *
+ * ISSUER_BASE_URL=https://oidc.integration.account.gov.uk (or similar)
+ * CREDENTIAL_ISSUER_URL=https://identity.integration.account.gov.uk (ditto)
+ * BASE_URL=https://YOUR_PROTOTYPE_ROOT_URL
+ * CLIENT_ID=YOUR_CLIENT_ID
+ * SECRET=LONG_RANDOM_VALUE (for example `openssl rand -base64 32`)
+ * RSA_PRIVATE_KEY="-----BEGIN RSA PRIVATE KEY-----
+ *    ...
+ * -----END RSA PRIVATE KEY-----"
+ *
+ * Optional environment variables:
+ *
+ * REQUEST_IDENTITY_PROOFING=true
+ *
+ * which makes the following required:
+ *
+ * SPOT_PUBLIC_KEY=PUBLIC_KEY_FROM_IDENTITY_PROOFING
+ *
+ */
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+const { auth, requiresAuth } = require('express-openid-connect')
+const jwt = require('jsonwebtoken')
+
+const authconfig = {
+  authorizationParams: {
+    response_type: 'code',
+    scope: 'openid email phone',
+  },
+  authRequired: false, // Specify `requiresAuth()` on protected routes instead
+  clientAssertionSigningKey: process.env.RSA_PRIVATE_KEY,
+  idpLogout: true,
+  idTokenSigningAlg: 'ES256',
+  routes: {
+    login: false, // so we can override `returnTo`
+  }
+}
+
+if (process.env.REQUEST_IDENTITY_PROOFING == "true") {
+
+  const claims = {
+    userinfo: {
+      "https://vocab.account.gov.uk/v1/coreIdentityJWT": {
+          essential: true
+      }
+    }
+  }
+
+  const vtr = ["P2.Cl.Cm"]
+
+  authconfig.authorizationParams.vtr = JSON.stringify(vtr)
+  authconfig.authorizationParams.claims = JSON.stringify(claims)
+}
+
+router.use(auth(authconfig))
+
+router.get('/login', (req, res) => res.oidc.login({ returnTo: '/profile' }))
+
+router.get('/profile', requiresAuth(), async (req, res, next) => {
+
+  const userinfo = await req.oidc.fetchUserInfo()
+  const core_id_jwt = userinfo["https://vocab.account.gov.uk/v1/coreIdentityJWT"]
+
+  if (core_id_jwt) {
+
+    const pubkey = process.env.SPOT_PUBLIC_KEY
+    const verification_options = {
+      algorithms: ["ES256"],
+      issuer: process.env.CREDENTIAL_ISSUER_URL,
+      subject: userinfo.sub
+    }
+
+    jwt.verify(core_id_jwt, pubkey, verification_options, (err, decoded) => {
+      if (err) {
+        next(`Could not validate coreIdentityJWT: ${err}`)
+      }
+      userinfo.core_identity = decoded // so the "profile" template can parse it
+    })
+  } // else core_id_jwt not present. This shouldn't be an error condition
+    // because if we requested it, we would get it. If the user fails to
+    // prove their identity, they would be stopped with an error before
+    // being returned to the Relying Party.
+
+  res.locals.user = userinfo
+  res.render('profile')
+})
